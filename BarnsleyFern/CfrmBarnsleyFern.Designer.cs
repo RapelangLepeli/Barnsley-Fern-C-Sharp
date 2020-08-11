6b9787fd1574906d7775849771c73f327f45001d
RapelangLepeli <50484534+RapelangLepeli@users.noreply.github.com>
2020-08-12 01:14:20 +0200

    Add files via upload

---
+++ BarnsleyFern/CfrmBarnsleyFern.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace BarnsleyFern
+{
+    partial class CfrmBarnsleyFern
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlDraw = new System.Windows.Forms.Panel();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlDraw
+            // 
+            this.pnlDraw.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pnlDraw.Location = new System.Drawing.Point(4, 3);
+            this.pnlDraw.Name = "pnlDraw";
+            this.pnlDraw.Size = new System.Drawing.Size(600, 650);
+            this.pnlDraw.TabIndex = 0;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(253, 659);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // CfrmBarnsleyFern
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 690);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.pnlDraw);
+            this.Name = "CfrmBarnsleyFern";
+            this.Text = "FERN";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDraw;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ BarnsleyFern/CfrmBarnsleyFern.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarnsleyFern
+{
+    public partial class CfrmBarnsleyFern : Form
+    {
+        static Random ProbabilitySelector;
+        static double X = 0, Y = 0;
+        static Graphics draw;
+        public CfrmBarnsleyFern()
+        {
+            InitializeComponent();
+            draw = pnlDraw.CreateGraphics();
+            ProbabilitySelector = new Random((int)DateTime.Now.Ticks);
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            SolidBrush solid = new SolidBrush(Color.LawnGreen);
+
+            for (int i = 0; i < 100000; i++)
+            {
+                int Select = ProbabilitySelector.Next(100);
+                double tempX = X;
+
+                if (Select <1)
+                {
+                    X = 0;
+                    Y = 0.16 * Y;
+                }
+                else if (Select < 86)
+                {
+                    X = 0.85 * X + 0.04 * Y;
+                    Y = -0.04 * tempX + 0.85 * Y + 1.6;
+                }
+                else if (Select < 93)
+                {
+                    X = 0.2 * X - 0.26 * Y;
+                    Y = 0.23 * tempX + 0.22 * Y + 1.6;
+                }
+                else
+                {
+                    X = -0.15 * X + 0.28 * Y;
+                    Y = 0.226 * tempX + 0.24 * Y + 0.44;
+                }
+                draw.FillEllipse(solid,(int)(300 + 50 * X),(int)(43 * Y),3,3);
+
+            }
+        }
+    }
+}
